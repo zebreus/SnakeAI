b7e5f6c810e304ca31ada8d5f561695c7a5efff9
Zebreus <lennarteichhorn@googlemail.com>
2023-04-06 16:35:49 +0200

    Add nixgl wrapper

--- flake.nix
+++ flake.nix
@@ -4,14 +4,25 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+    nixgl = {
+      url = "github:guibou/nixgl";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixgl }:
     flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nixglPkgs = nixgl.packages.${system};
+      in
       rec {
         name = "snakeai";
         packages.snakeai = import ./default.nix { pkgs = nixpkgs.legacyPackages.${system}; };
-        packages.default = packages.snakeai;
+        packages.wrapped = pkgs.writeShellScriptBin "snakeai" ''
+          ${nixglPkgs.default}/bin/nixGL ${packages.snakeai}/bin/SnakeAI
+        '';
+        packages.default = packages.wrapped;
       }
     );
 }
